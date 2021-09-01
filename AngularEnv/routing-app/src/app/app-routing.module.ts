d83aa035c49199a513e907d03fbe1f97a14516d8
RayRayYa <xic7682100@gmail.com>
2021-09-01 23:58:07 +0800

    Angular route application

---
+++ AngularEnv/routing-app/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+
+//# Defined the url path for the route.
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'main', component: MainComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
